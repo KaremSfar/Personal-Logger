51e13ef59d6c284eff35dc3453d8eb47a89bbcbe
Karem Sfar <karemsfar@gmail.com>
2020-06-28 18:26:57 +0100

    Migration success, Add LogCategoryId in MyLogs

---
+++ Migrations/202006281722257_alou.cs
@@ -0,0 +1,18 @@
+namespace PersonalLogger.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class alou : DbMigration
+    {
+        public override void Up()
+        {
+           
+        }
+        
+        public override void Down()
+        {
+           
+        }
+    }
+}
