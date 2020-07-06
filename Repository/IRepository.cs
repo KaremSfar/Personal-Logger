b2e82b802e98299182f542ecba08f46f0f0a7849
Karem Sfar <karemsfar@gmail.com>
2020-07-06 05:12:03 +0100

    Add IRepository

---
+++ Repository/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalLogger.Repository
+{
+    interface IRepository<TEntity> where TEntity : class
+    {
+    }
+}
