1cd3c1656b2a49c25085b5cf7ef8df9cfceb88d7
Karem Sfar <karemsfar@gmail.com>
2020-07-07 07:18:59 +0100

    Add IRepo, Repo, IUnitOfWork, UnitOfWork

---
+++ Repository/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using PersonalLogger.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalLogger.Repository
+{
+    public interface IUnitOfWork
+    {
+        IRepository<MyLog> MyLogs { get; }
+        IRepository<LogCategory> LogCategories { get; }
+        IRepository<FieldType> FieldTypes { get; }
+    }
+}
---
+++ Repository/UnitOfWork.cs
@@ -0,0 +1,46 @@
+﻿using PersonalLogger.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PersonalLogger.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private ApplicationDbContext _dbContext;
+        private Repository<MyLog> _myLogs;
+        private Repository<LogCategory> _logCategories;
+        private Repository<FieldType> _fieldTypes;
+
+        public UnitOfWork(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IRepository<MyLog> MyLogs
+        {
+            get
+            {
+                return _myLogs ?? (_myLogs = new Repository<MyLog>(_dbContext));
+            }
+        }
+
+        public IRepository<LogCategory> LogCategories
+        {
+            get
+            {
+                return _logCategories ?? (_logCategories = new Repository<LogCategory>(_dbContext));
+            }
+        }
+
+        public IRepository<FieldType> FieldTypes
+        {
+            get
+            {
+                return _fieldTypes ?? (_fieldTypes = new Repository<FieldType>(_dbContext));
+            }
+        }
+
+    }
+}
